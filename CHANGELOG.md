08e3fa364531dd2f434fc02cbea5077377ecd693
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-02 21:59:19 +0100

    v0.1a2

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1a2
+
+* bugfix where `Polygon2D.to_path` would loop infinitely
--- geom3d/__init__.py
+++ geom3d/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1a1'
+__version__ = '0.1a2'
 
 from .basic import Line, PointInLine, Vector, Box
 from .paths import Path
