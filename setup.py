2eebad54d982a18e071fe8f9ad0a6c0bdd190c38
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-24 21:24:57 +0100

    fix install deps

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext_modules = build([
 
 setup(keywords=['geometry', '3d', 'flight', 'path'],
       packages=['geom3d', 'geom3d.degrees', 'geom3d.meshes', 'geom3d.paths', 'geom3d.polygons'],
-      version='0.4_a2',
+      version='0.4_a3',
       install_requires=[
             'satella',
       ],
