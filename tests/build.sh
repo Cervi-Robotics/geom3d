2b860ef4296374636bd5ca99f9946d505b37dc4c
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-27 21:19:37 +0100

    build for manylinux

---
+++ tests/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ $1 == "travis" ]; then
+  docker pull "$DOCKER_IMAGE"
+  docker run --rm -it -e PLAT=$PLAT -e PYPI_USER="$PYPI_USER" -e PYPI_PWD="$PYPI_PWD" -v `pwd`:/io $DOCKER_IMAGE bash /io/tests/build.sh
+else
+
+  yum install -y atlas-devel
+  cd /io
+
+  # Compile wheels
+  for PYBIN in /opt/python/*/bin; do
+      "${PYBIN}/pip" install -r /io/requirements.txt
+      "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+  done
+
+  # Bundle external shared libraries into the wheels
+  for whl in wheelhouse/*.whl; do
+      auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+  done
+
+  # Install packages and test
+  for PYBIN in /opt/python/*/bin/; do
+      "${PYBIN}/pip" install geom3d --no-index -f /io/wheelhouse
+      (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+  done
+
+  pip install twine
+  twine upload -u "$PYPI_USER" -p "$PYPI_PWD" wheelhouse/*
+
+fi
