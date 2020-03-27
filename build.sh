aaf554be6dd1b798791788efd92d69f08195353e
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-27 20:41:17 +0100

    build for manylinux

--- build.sh
+++ build.sh
@@ -3,20 +3,9 @@
 set -x
 set -e
 
-pip install coverage
-pip install -r requirements.txt
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
-python setup.py test
-coverage xml
-./cc-test-reporter after-build -t coverage.py --exit-code $TRAVIS_TEST_RESULT
-
-if [ $TRAVIS_BRANCH == "master" ]; then
-    if [ $BUILD == "true" ]; then
-      docker pull $DOCKER_IMAGE
-      docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/travis/build-wheels.sh
-      cd wheelhouse
-      twine upload -u "$PYPI_USER" -p "$PYPI_PWD" *
-fi
+pip install twine
+docker pull $DOCKER_IMAGE
+docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/travis/build-wheels.sh
+cd wheelhouse
+twine upload -u "$PYPI_USER" -p "$PYPI_PWD" *
 
---
+++ tests/test.sh
@@ -0,0 +1,14 @@
+set -x
+set -e
+
+pip install coverage
+pip install -r requirements.txt
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+python setup.py test
+TRAVIS_TEST_RESULT=$?
+coverage xml
+./cc-test-reporter after-build -t coverage.py --exit-code ${TRAVIS_TEST_RESULT}
+exit $TRAVIS_TEST_RESULT
+
