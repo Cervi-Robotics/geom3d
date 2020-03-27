e8fcf3b156369807dca5d4be224b761ea8403c1a
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-27 20:17:07 +0100

    build automatically

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 from snakehouse import Multibuild, build
 from setuptools import Extension
+from satella.files import find_files
+
 
 ext_modules = build([
       Extension('geom3d.base', ['geom3d/base.pyx']),
       Extension('geom3d.basic', ['geom3d/basic.pyx']),
-      Multibuild('geom3d.degrees', ['geom3d/degrees/planets.pyx',
-                                    'geom3d/degrees/coordinates.pyx']),
-      Multibuild('geom3d.paths', ['geom3d/paths/nonintersecting.pyx',
-                                  'geom3d/paths/polygon.pyx',
-                                  'geom3d/paths/path.pyx']),
-      Extension('geom3d.polygons.twodimensional', ['geom3d/polygons/twodimensional.pyx']),
-      Multibuild('geom3d.meshes', ['geom3d/meshes/meshes.pyx']),
+      Multibuild('geom3d.degrees', find_files('geom3d/degrees', r'(.*)\.pyx')),
+      Multibuild('geom3d.paths', find_files('geom3d/paths', r'(.*)\.pyx')),
+      Multibuild('geom3d.polygons', find_files('geom3d/polygons', r'(.*)\.pyx')),
+      Multibuild('geom3d.meshes', find_files('geom3d/meshes', r'(.*)\.pyx')),
 ], compiler_directives={
       'language_level': '3'
 })
