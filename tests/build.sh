620feb1a2f9412d4a22eaa7d83a8476fc142683f
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-14 11:50:57 +0200

    fix build manylinux

--- tests/build.sh
+++ tests/build.sh
@@ -4,7 +4,9 @@ set -x
 set -e
 
 if [ "$1" == "travis" ]; then
-  docker run --rm -it -e PYPI_USER="$PYPI_USER" -e PYPI_PWD="$PYPI_PWD" -e PLAT=$PLAT -v `pwd`:/io "$DOCKER_IMAGE" bash /io/tests/build.sh
+  docker run --rm -it -e PLAT=$PLAT -v `pwd`:/io "$DOCKER_IMAGE" bash /io/tests/build.sh
+  pip install twine
+  twine upload -u "$PYPI_USER" -p "$PYPI_PWD" "wheelhouse/geom3d-*${PLAT}.whl"
 else
   # Executed in target dockerized CentOS 5 environment
   rm -rf /opt/python/cp27-cp27m
@@ -26,6 +28,4 @@ else
   for whl in wheelhouse/*.whl; do
       auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
   done
-  /opt/python/cp38-cp38/bin/pip install twine
-  /opt/python/cp38-cp38/bin/twine upload -u "$PYPI_USER" -p "$PYPI_PWD" "wheelhouse/geom3d-*${PLAT}.whl"
 fi
