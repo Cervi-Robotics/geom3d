a8047eff518291ea8b2bac882c9fb09441e863b4
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-06-04 17:59:42 +0200

    changes, so now it (hopefully) works

--- tests/test_paths/test_nonintersecting.py
+++ tests/test_paths/test_nonintersecting.py
@@ -28,3 +28,5 @@ class TestNonintersecting(unittest.TestCase):
                               MakeNonintersectingPaths(0, 100, path2)])
         self.assertGreater(path2.length, 400)
         self.assertTrue(are_mutually_nonintersecting([path1, path2]))
+        path1.simplify()
+        path2.simplify()
