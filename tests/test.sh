020d49c2746f4551bc5a8f684ea08b2c4112a31a
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-06-04 14:36:36 +0200

    fix tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,12 +2,12 @@ set -x
 set -e
 
 pip install --upgrade pip
-pip install coverage nose2
+pip install coverage
 pip install -r requirements.txt
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
 ./cc-test-reporter before-build
-coverage run -m nose2 -vvv
+python setup.py test
 TRAVIS_TEST_RESULT=$?
 coverage xml
 ./cc-test-reporter after-build -t coverage.py --exit-code ${TRAVIS_TEST_RESULT}
