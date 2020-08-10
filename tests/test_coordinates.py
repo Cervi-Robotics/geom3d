67581e1290060372d955037f8328361cc7d975b0
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-08-10 14:07:09 +0200

    extra tests

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -1,9 +1,8 @@
 import geom3d.degrees
 import unittest
-from geom3d.degrees import XYPointCollection, Coordinates
 
 
 class TestCoordinates(unittest.TestCase):
     def test_xypoint_collection(self):
-        coords = [Coordinates(0.0, 0.3), Coordinates(0.1, 0.2)]
-        XYPointCollection(coords)
+        coords = [geom3d.degrees.Coordinates(0.0, 0.3), geom3d.degrees.Coordinates(0.1, 0.2)]
+        geom3d.degrees.XYPointCollection(coords)
